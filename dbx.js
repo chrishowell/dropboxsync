35e493b157957e5eebfce97243c9f3297f3e1a93
Chris Howell <chris@howell.xyz>
2016-07-28 22:27:49 +0100

    Add credentials to gitignore

---
+++ dbx.js
@@ -0,0 +1,42 @@
+var Dropbox = require('dropbox');
+var dbx = new Dropbox({ accessToken: 'tffJxFV2gxAAAAAAAAAAsdxX8bFIT44uOQZO2x7D8-xiq0mzdhop8NIwyxOwVMKU' });
+
+// This would be done once and the cursor kept in Redis
+var latestCursor = '';
+dbx
+.filesListFolder({path: '', include_media_info: true, include_deleted: false})
+.then(response => {
+  // handleEntries(response.entries);
+  latestCursor = response.cursor;
+  }
+).catch(error => console.log(error));
+
+// This would be done using a CloudWatch Event trigger looking up the cursor in Redis
+setInterval(() =>
+  {
+    console.log('Performing Sync');
+    dbx
+    .filesListFolderContinue({cursor: latestCursor})
+    .then(response => {
+      handleEntries(response.entries);
+      latestCursor = response.cursor;
+    })
+    .catch(error => console.log(error));
+  }
+, 5000);
+
+function handleEntries(entries) {
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    if (entry[".tag"] == "deleted") {
+      console.log(entry.name + ' has been deleted.')
+      continue;
+    }
+    dbx.filesGetTemporaryLink({path: entry.path_lower})
+    .then(response =>
+      console.log(JSON.stringify(response, null, 2))
+    ).catch(error => console.log(error));
+  }
+}
+
+// Don't forget to handle the has_more, or will that be covered by the cursor?
