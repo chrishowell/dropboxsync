474e6470b1ded14ed3a9ccb0643e36f26bcfac4b
Chris Howell <chris@howell.xyz>
2016-07-29 00:25:11 +0100

    Add local Redis store for cursor

--- dbx.js
+++ dbx.js
@@ -1,40 +1,68 @@
+const CURSOR_KEY = "cursor";
+
 var Dropbox = require('dropbox');
 var fs = require('fs');
+var redis = require('redis');
 
 var credentialsFile = fs.readFileSync("credentials.json");
 var credentials = JSON.parse(credentialsFile);
 
 var dbx = new Dropbox({ accessToken: credentials.accessToken });
 
-// This would be done once and the cursor kept in Redis
-var latestCursor = '';
-dbx
-.filesListFolder({path: '', include_media_info: true, include_deleted: false})
-.then(response => {
-  // handleEntries(response.entries);
-  latestCursor = response.cursor;
-  }
-).catch(error => console.log(error));
+var redisClient = redis.createClient();
+redisClient.on("error", function (err) {
+    console.log("RedisError " + err);
+});
 
-// This would be done using a CloudWatch Event trigger looking up the cursor in Redis
-setInterval(() =>
-  {
-    console.log('Performing Sync');
-    dbx
-    .filesListFolderContinue({cursor: latestCursor})
-    .then(response => {
-      handleEntries(response.entries);
-      latestCursor = response.cursor;
-    })
-    .catch(error => console.log(error));
+redisClient.get(CURSOR_KEY, function(err, cursor) {
+  if (cursor == null) {
+    console.log('Fetching latest cursor from Dropbox');
+    fetchLatestCursorAnd(sync);
+  } else {
+    console.log('Fetched latest cursor from Redis');
+    sync(cursor);
   }
-, 5000);
+});
+
+function fetchLatestCursorAnd(callback) {
+  dbx
+  .filesListFolderGetLatestCursor({path: '', include_media_info: true, include_deleted: false})
+  .then(response => callback(response.cursor))
+  .catch(error => console.log(error));
+}
+
+function saveLatestCursor(cursor) {
+  console.log('Saving cursor ' + cursor);
+  redisClient.set(CURSOR_KEY, cursor);
+}
+
+function sync(cursor) {
+  var latestCursor = cursor;
+  // This repeat will be done using a CloudWatch Event trigger looking up the cursor in Redis
+  setInterval(() =>
+    {
+      console.log('Performing Sync with cursor ' + latestCursor);
+      dbx
+      .filesListFolderContinue({cursor: latestCursor})
+      .then(response => {
+        handleEntries(response.entries);
+        latestCursor = response.cursor;
+        saveLatestCursor(response.cursor);
+      })
+      .catch(error => console.log(error));
+    }
+  , 5000);
+}
 
 function handleEntries(entries) {
-  for (var i = 0; i < entries.length; i++) {
+  var entryCount = entries.length;
+  if (entryCount > 0) {
+    console.log("Found " + entryCount + " update since last sync.");
+  }
+  for (var i = 0; i < entryCount; i++) {
     var entry = entries[i];
     if (entry[".tag"] == "deleted") {
-      console.log(entry.name + ' has been deleted.')
+      console.log(entry.name + ' has been deleted since last sync.');
       continue;
     }
     dbx.filesGetTemporaryLink({path: entry.path_lower})
